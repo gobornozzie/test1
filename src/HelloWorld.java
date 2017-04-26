49e0ccb018767b038f457e9c2a5aa884ca00b7a5
HOME\Garry <gtoborn@hotmail.com>
2017-04-25 20:53:46 -0600

    new commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World in test 1 project");
+		System.out.println("another comment goes here");
 
 	}
 
