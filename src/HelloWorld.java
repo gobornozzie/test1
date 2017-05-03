ec90ba598c26e78a8608922867e140885193b90e
HOME\Garry <gtoborn@hotmail.com>
2017-05-02 20:38:34 -0600

    commit message here

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World in test 1 project");
 		System.out.println("another comment goes here");
+		System.out.println("third comment is the charm");
 
 	}
 
